9dc07895365e6fc0391b7fd6893ace2b95fb7529
sw <seansonnysw@gmail.com>
2023-05-23 00:00:49 +0900

    toast

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Home from './components/Home';
+import { ToastContainer, toast } from 'react-toastify';
+
 
 const App = () => {
   return (
     <div>
       {/* <h1>Wedding Invitation App</h1> */}
+      <ToastContainer />
       <Home />
     </div>
   );
